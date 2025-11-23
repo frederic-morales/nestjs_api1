e59723f073cecd115fc4346f7d825d6419503f53
frederic-morales <tocomorales30@gmail.com>
2025-11-22 18:37:22 -0600

    Creando entidad Category - clase 19

--- src/posts/services/posts.service.ts
+++ src/posts/services/posts.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
-import { Post } from './entities/post.entity';
+import { CreatePostDto } from '../dto/create-post.dto';
+import { UpdatePostDto } from '../dto/update-post.dto';
+import { Post } from '../entities/post.entity';
 
 @Injectable()
 export class PostsService {
